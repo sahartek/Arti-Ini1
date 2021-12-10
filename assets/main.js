1920cba69f3c92b7cc783e52ddad4d968b85d716
sahartek <sahar_tekaya@yahoo.fr>
2021-12-10 15:26:19 +0100

    2

---
+++ assets/main.js
@@ -0,0 +1,41 @@
+/*====== SHOW MENU ======= *//*==================== SHOW MENU ====================*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+      navToggle.addEventListener('click', () =>{
+            navMenu.classList.add('show-menu')
+      })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+     navClose.addEventListener('click', () =>{
+          navMenu.classList.remove('show-menu')
+     })
+}
+
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+window.addEventListener('scroll', (event) => {
+      let element = document.getElementById("navbar");
+      if (window.pageYOffset < 20 ){
+            element.classList.add("bg-transp");
+      }
+      else{
+            element.classList.remove("bg-transp");
+      }
+  });
